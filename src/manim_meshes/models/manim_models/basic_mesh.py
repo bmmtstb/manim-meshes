fixed vertices rendering for ManimMesh, removed 'scene' attribute of ManimMesh to fix SuzanneScene (else it is not possible to use 'deepcopy' which is used by Manim for animations, e.g. rotation)

src/manim_meshes/models/manim_models/basic_mesh.py
@@ -68,7 +68,7 @@ from manim_meshes.models.manim_models.params import get_param_or_default, M2DM, 
 #         raise NotImplementedError
 
 # pylint: disable=too-many-instance-attributes
-class ManimMesh(m.VGroup, metaclass=ConvertToOpenGL):
+class ManimMesh(m.Group, metaclass=ConvertToOpenGL):
     """
     another Mesh implementation, a little bit faster + looks better
     -> FIXME has no vertex dots, necessary?
@@ -76,10 +76,9 @@ class ManimMesh(m.VGroup, metaclass=ConvertToOpenGL):
     inspired by manim class 'Surface'
     """
 
-    def __init__(self, scene: m.Scene, mesh: Mesh, *args, **kwargs) -> None:
+    def __init__(self, mesh: Mesh, *args, **kwargs) -> None:
         super().__init__(*args)
         self.mesh: Mesh = mesh
-        self.scene: m.Scene = scene
         self.vertices: m.VGroup = m.VGroup()
         self.edges: m.VGroup = m.VGroup()
         self.faces: m.VGroup = m.VGroup()
@@ -115,10 +114,10 @@ class ManimMesh(m.VGroup, metaclass=ConvertToOpenGL):
     def _setup_vertices(self):
         """set the vertices as 3D manim objects"""
         if self.clear_vertices:
-            self.vertices = m.VGroup()
+            self.vertices = m.Group()
 
         for v in self.mesh.get_3d_vertices():
-            self.vertices.add(m.Sphere(v, radius=0.03, fill_color=self.verts_color, fill_opacity=1.,  stroke_width=0.))
+            self.vertices.add(m.Sphere(v, radius=0.05, color=self.verts_color))
 
     def _setup_edges(self):
         """set the edges as manim objects"""
@@ -135,7 +134,7 @@ class ManimMesh(m.VGroup, metaclass=ConvertToOpenGL):
         # for all edges at once
         self.edges.set_fill(
             color=self.edges_color,
-            opacity=1
+            opacity=1.
         )
         self.edges.set_stroke(
             color=self.edges_color,
@@ -211,24 +210,24 @@ class ManimMesh(m.VGroup, metaclass=ConvertToOpenGL):
         vert_2 = self.mesh.get_3d_vertices()[edge[1]]
         e.set_points_as_corners([vert_1, vert_2])
 
-    def shift_vertex(self, vertex_idx: int, shift: np.ndarray, **kwargs):
+    def shift_vertex(self, scene: m.Scene, vertex_idx: int, shift: np.ndarray, **kwargs):
         """shift vertex and update faces"""
         # expect everything has the same dimensions as mesh.dim
         start = self.mesh.get_vertices()[vertex_idx].copy()
         tracker = m.ValueTracker(0)
         tracker.add_updater(lambda mo: self._update_vertex(vertex_idx, start + tracker.get_value() * shift, **kwargs))
-        self.scene.add(tracker)
-        self.scene.play(tracker.animate(**kwargs).set_value(1))
-        self.scene.remove(tracker)
+        scene.add(tracker)
+        scene.play(tracker.animate(**kwargs).set_value(1))
+        scene.remove(tracker)
 
-    def move_vertices_to(self, new_positions: np.ndarray, **kwargs):
+    def move_vertices_to(self, scene: m.Scene, new_positions: np.ndarray, **kwargs):
         """visually move all vertices to new positions and update faces"""
         if len(new_positions) != self.mesh.nof_vertices:
             raise InvalidShapeException("new_positions", len(new_positions), self.mesh.nof_vertices)
         for i, new_pos in enumerate(new_positions):
-            self.move_vertex_to(vertex_idx=i, pos=new_pos, **kwargs)
+            self.move_vertex_to(scene, vertex_idx=i, pos=new_pos, **kwargs)
 
-    def move_vertex_to(self, vertex_idx: int, pos: np.ndarray, **kwargs):
+    def move_vertex_to(self,scene: m.Scene, vertex_idx: int, pos: np.ndarray, **kwargs):
         """visually move vertex to pos and update faces"""
         if vertex_idx < 0 or self.mesh.nof_vertices < vertex_idx:
             raise InvalidVertexIndex(vertex_idx, self.mesh.nof_vertices)
@@ -238,7 +237,7 @@ class ManimMesh(m.VGroup, metaclass=ConvertToOpenGL):
         current_pos = self.mesh.get_vertices()[vertex_idx].copy()
         shift = pos - current_pos
         # use shift method to slowly move point to desired place
-        self.shift_vertex(vertex_idx, shift, **kwargs)
+        self.shift_vertex(scene, vertex_idx, shift, **kwargs)
 
     def move_to_grid(self, scene: m.Scene, grid_sizes: Tuple[float, ...], threshold: Tuple[float, ...], nof_steps: int):
         """slowly snap to a given grid, uses stepwise mesh.snap_to_grid()"""
@@ -247,7 +246,7 @@ class ManimMesh(m.VGroup, metaclass=ConvertToOpenGL):
             old_mesh: Mesh = copy.copy(self.mesh)
             old_mesh.snap_to_grid(grid_sizes, threshold, step)
             # use new calculated positions but have still the old mesh
-            self.move_vertices_to(self.scene, old_mesh.get_vertices())
+            self.move_vertices_to(scene, old_mesh.get_vertices())
             scene.wait(0.5)
 
 
@@ -261,7 +260,7 @@ class Manim2DMesh(ManimMesh):
      mesh functionalities.
     """
 
-    def __init__(self, scene: m.Scene, mesh: Mesh, *args, **kwargs) -> None:
+    def __init__(self, mesh: Mesh, *args, **kwargs) -> None:
         if mesh.dim == 3:
             if np.sum(np.abs(mesh.get_vertices()[:, 2] != 0)):
                 raise InvalidMeshException("Mesh has z values != 0 and therefore is not 2D.")
@@ -271,7 +270,6 @@ class Manim2DMesh(ManimMesh):
         # init ManimMesh
         super().__init__(
             mesh=mesh,
-            scene=scene,
             display_vertices=get_param_or_default("display_vertices", kwargs, M2DM),
             display_edges=get_param_or_default("display_edges", kwargs, M2DM),
             display_faces=get_param_or_default("display_faces", kwargs, M2DM),
@@ -310,7 +308,7 @@ class Manim2DMesh(ManimMesh):
         circ.shift(center)
         return circ
 
-    def edge_flip(self, face_idx_1: int, face_idx_2: int, **kwargs):
+    def edge_flip(self,scene: m.Scene, face_idx_1: int, face_idx_2: int, **kwargs):
         """
         Flips the edge shared by the given triangles. Raises an error if the faces are not triangles
         or do not share exactly one edge.
@@ -348,7 +346,7 @@ class Manim2DMesh(ManimMesh):
         new_edge = old_edge.copy()
         new_edge.set_points_as_corners([self.mesh.get_3d_vertices()[v_1], self.mesh.get_3d_vertices()[v_2]])
         anims.append(old_edge.animate(**kwargs).become(new_edge))
-        self.scene.play(*anims)
+        scene.play(*anims)
 
     def get_points_violating_delaunay(self, face_id: int):
         """given a triangle by id, get all points violating delaunay criterion"""

tests/test_scene.py
@@ -19,14 +19,12 @@ class ConeScene(m.ThreeDScene):
     """Display a cone"""
 
     def construct(self):
-        self.set_camera_orientation(phi=70 * m.DEGREES, zoom=0.40)
+        self.set_camera_orientation(phi=70 * m.DEGREES)
         mesh = create_model(name="tail_topper")
-        mesh.scale_mesh(scaling=0.3)
-        mesh.translate_mesh(np.array([0, -8, 0]))
+        mesh.scale_mesh(scaling=0.2)
+        mesh.translate_mesh(np.array([0,-5,0]))
         manim_mesh_obj = ManimMesh(
-            scene=self,
-            mesh=mesh,
-            display_vertices=True,
+            mesh=mesh
         )
         self.add(manim_mesh_obj)
 
@@ -37,7 +35,7 @@ class SuzanneScene(m.ThreeDScene):
     def construct(self):
         self.camera.set_phi(90 * m.DEGREES)
         mesh = create_model(name="suzanne")
-        manim_mesh_obj = ManimMesh(scene=self, mesh=mesh)
+        manim_mesh_obj = ManimMesh(mesh=mesh)
         self.add(manim_mesh_obj)
         self.play(
             m.Rotate(
@@ -56,7 +54,7 @@ class PyramidScene(m.ThreeDScene):
     def construct(self):
         self.set_camera_orientation(70 * m.DEGREES, 30 * m.DEGREES)
         mesh = create_pyramid()
-        manim_mesh_obj = ManimMesh(scene=self, mesh=mesh, display_vertices=True)
+        manim_mesh_obj = ManimMesh(mesh=mesh, display_vertices=True)
         self.add(manim_mesh_obj)
         self.play(
             manim_mesh_obj.get_face(0).animate.set_fill(m.RED, 1)
@@ -76,7 +74,7 @@ class TriangleScene(m.ThreeDScene):
         text.fix_in_frame()
         self.add(text)
         mesh = create_coplanar_triangles()
-        mesh_2d = Manim2DMesh(scene=self, mesh=mesh)
+        mesh_2d = Manim2DMesh(mesh=mesh)
         self.add(mesh_2d)
         triangle = mesh_2d.get_face(0)
         self.play(triangle.animate.set_fill(m.YELLOW_D, None))  # mark triangle
@@ -91,7 +89,7 @@ class TriangleScene(m.ThreeDScene):
             if not mesh_2d.is_point_violating_delaunay(indices[0], 0) else None)
         # use mesh_2d.move_vertex_to and mesh_2d.shift_vertex instead of e.g. self.play(points[0].animate.move_to)
         # -> otherwise the faces will not be updated
-        mesh_2d.shift_vertex(indices[0], 0.35 * m.DL[:2])
+        mesh_2d.shift_vertex(self, indices[0], 0.35 * m.DL[:2])
         points[0].remove_updater(points[0].non_time_updaters[-1])  # remove last updater
         self.play(m.FadeOut(points[0]), m.Uncreate(circle))
         self.play(triangle.animate.set_fill(mesh_2d.faces_color, None))  # unmark triangle
@@ -108,7 +106,7 @@ class TriangleScene(m.ThreeDScene):
         triangle_a = mesh_2d.get_face(2)
         triangle_b = mesh_2d.get_face(3)
         self.play(triangle_a.animate.set_fill(m.YELLOW_D, None), triangle_b.animate.set_fill(m.YELLOW_D, None))
-        mesh_2d.edge_flip(2, 3)
+        mesh_2d.edge_flip(self, 2, 3)
         circle_a = mesh_2d.get_circle(2)  # circumcircle around triangle
         circle_b = mesh_2d.get_circle(3)  # circumcircle around triangle
         self.play(m.Create(circle_a), m.Create(circle_b), self.camera.animate.shift(m.DOWN))
@@ -129,7 +127,7 @@ class TriangleScene(m.ThreeDScene):
         anims_remove.append(m.Uncreate(circle_b))
         self.play(*anims_remove)
         # flip
-        mesh_2d.edge_flip(2, 3)
+        mesh_2d.edge_flip(self, 2, 3)
         # get new circles
         circle_a = mesh_2d.get_circle(2)  # circumcircle around triangle
         circle_b = mesh_2d.get_circle(3)  # circumcircle around triangle
@@ -151,7 +149,6 @@ class SnapToGridScene(m.ThreeDScene):
     def construct(self):
         self.set_camera_orientation(0, 0)
         grid_mesh = Manim2DMesh(
-            scene=self,
             mesh=create_grid([(-3, 3, 7), (-3, 3, 7)]),
             display_vertices=False,
             faces_stroke_width=0.3,
@@ -160,7 +157,6 @@ class SnapToGridScene(m.ThreeDScene):
         self.add(grid_mesh)
         # generate "random" points
         vertex_mesh = Manim2DMesh(
-            scene=self,
             mesh=Mesh(
                 verts=np.array([[1, 1], [0.2, 0.1], [0.5, 0.7], [-1, 1.1], [-2.5, -2.3], [-1.1, 0.2], [2.1, 1.7]]),
                 faces=None,